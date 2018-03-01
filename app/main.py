cee4c7cdd7af479f2e7e6c8aa9ca1547adfe2688
ciaradillon16 <ciara.dillon1@ucdconnect.ie>
2018-03-01 19:03:11 +0000

    Attempting to restructure code into object-oriented Python

--- app/main.py
+++ app/main.py
@@ -1,19 +1,41 @@
-class lightTester:
+#def main(filename, N): #files provided and first line of that file = N  
     
-    lights = ""
+    #lights = lightTester(N)
+
+    #instructions = parse_file(filename)
     
-    def __init__(self, N):
-        self.lights = ""
+    #for cmd in instructions:
+        #lights.apply(cmd)
+
+
+#class lightTester:
+    
+lights = [[0 for x in range(10)] for y in range(10)]
+    
+    #def __init__(self, N):
+        #self.lights = ""
         
-    def apply(self, cmd):
-        if (cmd =='turn on'):
-            pass
-        elif (cmd == 'turn off'):
-            pass
-        elif (cmd == 'switch'):
-            pass
+    #def apply(self, cmd):
+        #if (cmd =='turn on'):
+            #pass
+        #elif (cmd == 'turn off'):
+            #pass
+        #elif (cmd == 'switch'):
+            #pass
+      
         
-    def count(self):
-        count = ""
+def count(lights):
+    count = 0
+    for i in range(len(lights)):
+        for j in range(len(lights[i])):
+            if lights[i][j] == True:
+                count += 1 
         return count
+
+for i in range(len(lights)):
+    for j in range(len(lights[i])):
+        lights[i][j] = False #False - off 
+
+lights[0][4] = True  
+print(count(lights))    
             

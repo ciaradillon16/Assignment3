086ae8da4e4bfb343e07264e2d9ae523c15f7449
ciaradillon16 <ciara.dillon1@ucdconnect.ie>
2018-03-01 21:26:21 +0000

    Testing code as a class with basic hardcoded inputs

--- app/main.py
+++ app/main.py
@@ -2,40 +2,40 @@
     
     #lights = lightTester(N)
 
+
     #instructions = parse_file(filename)
-    
     #for cmd in instructions:
         #lights.apply(cmd)
-
-
-#class lightTester:
-    
-lights = [[0 for x in range(10)] for y in range(10)]
-    
-    #def __init__(self, N):
-        #self.lights = ""
-        
+class lightTester:
+ 
+    lights = None
+    N = 10 
+     
+    def __init__(self, N):
+        self.lights = [[False]*N for i in range(N)]
+                 
     #def apply(self, cmd):
         #if (cmd =='turn on'):
-            #pass
+    def apply(self):
+        for i in range(len(self.lights)):
+            for j in range(len(self.lights[i])):
+                self.lights[i][j] = False               
+        return self.lights
+             
         #elif (cmd == 'turn off'):
             #pass
         #elif (cmd == 'switch'):
             #pass
-      
         
-def count(lights):
-    count = 0
-    for i in range(len(lights)):
-        for j in range(len(lights[i])):
-            if lights[i][j] == True:
-                count += 1 
+    def count(self,lights):
+        count = 0
+        for i in range(len(self.lights)):
+            for j in range(len(self.lights[i])):
+                if lights[i][j] == True:
+                    count += 1 
         return count
-
-for i in range(len(lights)):
-    for j in range(len(lights[i])):
-        lights[i][j] = False #False - off 
-
-lights[0][4] = True  
-print(count(lights))    
-            
+  
+lights = [[0 for x in range(10)] for y in range(10)]
+x = lightTester(10)
+print(lightTester.apply(x))
+print(lightTester.count(x, lights))      

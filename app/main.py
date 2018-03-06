10a7d39b3395b17dcb66464c9df1db7383660a13
ciaradillon16 <ciara.dillon1@ucdconnect.ie>
2018-03-06 18:32:31 +0000

    Code running with one input, attempting to fix errors with count being off

--- app/main.py
+++ app/main.py
@@ -4,61 +4,86 @@ import re
 #def main(filename, N): #files provided and first line of that file = N  
     
     #lights = lightTester(N)
+#     instructions = lights.parse_file(filename)
+#     
+#     for cmd in instructions:
+#         lights.apply(cmd)
 
-
-    #instructions = parse_file(filename)
-    #for cmd in instructions:
-        #lights.apply(cmd)
-class lightTester:
- 
-    lights = None
-    N = 10 
-     
-    def __init__(self, N):
-        self.lights = [[False]*N for i in range(N)]
-                 
-    #def apply(self, cmd):
-        #if (cmd =='turn on'):
-    def apply(self):
-        for i in range(len(self.lights)):
-            for j in range(len(self.lights[i])):
-                self.lights[i][j] = False               
-        return self.lights
-             
-        #elif (cmd == 'turn off'):
-            #pass
-        #elif (cmd == 'switch'):
-            #pass
         
-    def count(self,lights):
+class lightTester:
+    
+    lights = None
+   
+    def __init__(self, N):
+        self.N = N
+        self.lights = [[False]*N for _ in range(N)]
+        
+    def size(self, number):
+        if number < 0:
+            return 0
+        if number > self.N:
+            return self.N-1
+        else:
+            return number
+                         
+    def apply(self, array):
+        self.array = array
+        cmd = array[0]
+        x = self.size(int(array[1]))
+        y = self.size(int(array[2]))
+        x1 = self.size(int(array[3]))
+        y1 = self.size(int(array[4]))
+    
+        if cmd == 'turn off' or cmd == 'turn off ':
+            for i in range(y, y1+1):
+                for j in range(x, x1+1):
+                    self.lights[i][j] = False                
+            #return self.lights
+        
+        elif cmd == 'turn on' or cmd == 'turn on ':
+            for i in range(y, y1+1):
+                for j in range(x, x1+1):
+                    self.lights[i][j] = True  
+            #return self.lights
+        
+        elif cmd == 'switch' or cmd == 'switch ':
+            for i in range(y, y1+1):
+                for j in range(x, x1+1):
+                    self.lights[i][j] = not self.lights[i][j]
+#                     if (self.lights[i][j] == False):
+#                         self.lights[i][j] = True
+#                     elif self.lights[i][j] == True:
+#                        self.lights = False
+        return self.lights
+                 
+    def count(self):
         count = 0
-        for i in range(len(self.lights)):
-            for j in range(len(self.lights[i])):
-                if lights[i][j] == True:
+        for i in range (self.N):
+            for j in range (self.N): 
+                if self.lights[i][j]:
                     count += 1 
         return count
     
-    def parse_file(self, filename):
-        self.link = input("Enter the list of inputs: ")
-        self.r = requests.get(self.link)
-        self.file = self.r.text.split('\n')
-        pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+def parse_file():
+#self.link = input("Enter the list of inputs: ")
 
-        for line in self.file[1:-1]:  
-            m = pat.match(line)
-            if m: 
-                array = [m.group(1), m.group(2), m.group(3), m.group(4), m.group(5)]
-                cmd = array[0]
-                x1 = array[1]
-                x2 = array[2]
-                y1 = array[3]
-                y2 = array[4]
-            print(cmd, x1, x2, y1, y2)
-
-
-
-
-#lights = [[0 for x in range(10)] for y in range(10)]
-#x = lightTester(10)
-#print(lightTester.apply(x))
-#print(lightTester.count(x, lights))      
+    r = requests.get("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+    file = r.text.split('\n')
+    N = file[0]
+    print(N)
+    pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+    
+    for line in file:  
+        m = pat.match(line)
+        print
+        if m: 
+            array = [m.group(1), m.group(2), m.group(3), m.group(4), m.group(5)]
+            print(array)
+        
+    mylight = lightTester(int(N))
+    mylight.apply(array)
+    print(mylight.count())
+    
+if __name__ == '__main__':
+    parse_file()
+    

42e4ac39294bdac460635e81761e2c907c984d83
ciaradillon16 <ciara.dillon1@ucdconnect.ie>
2018-03-08 10:13:57 +0000

    Fixing installation

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name="light_test",
+setup(name="Assignment3",
       version="0.1",
       description="Program to switch on and off lights on an LED board",
       url="",
       author="Ciara Dillon", 
       author_email="ciara.dillon1@ucdconnect.ie",
       license="GPL3",
-      packages=['Assignment3'],
+      packages=['app'],
       entry_points={
-          'console_scripts':['solve_led=app.main:main']
+          'console_scripts':['solve_led=app.main:parse_file']
           }
       ) 

efc6f50e7c61547a48a06e743c4054a6ad3f35f2
ciaradillon16 <ciara.dillon1@ucdconnect.ie>
2018-03-09 09:57:05 +0000

    Some simple tests to test the apply function of my lightTester class

---
+++ README.md
@@ -0,0 +1,15 @@
+Light-Tester Application || Assignment 3
+
+—----------------------------------------------
+
+This program is installed on your machine and takes in inputs to determine how many lights
+are switched on on a LED board of lights. 
+
+-----------------------------------------------
+
+To run this program: 
+- Copy the link to the GitHub repository
+- In your command line, run pip install git+https://github.com/ciaradillon16/Assignment3
+- Once it has installed, run the command solve_led —input with the file you wish to run and allow a few moments while the code executes. 
+
+Note: input file a will take about an hour to run due to the size of it. 
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,15 +1,30 @@
-import unittest
-import sys 
-
-sys.path.append('../src') 
-
-from app.main import *
-
 class Test(unittest.TestCase):
+#     
+    def test_apply_function(self):
+        test1 = lightTester(5)
+        test1.apply(["switch", 0, 0, 1, 1])
+        self.assertTrue(test1.lights[0][1] == 1)
+        
+        test2 = lightTester(3)
+        test2.apply(['turn off', 0, 2, 0 ,2])
+        self.assertTrue(test2.lights[0][2] == 0)
+        
+        test3 = lightTester(9)
+        test3.apply(["turn on", 0, 0, 2, 2])
+        self.assertTrue(test3.lights[0][1] == 1)
 
-    def turnOn(self):
-        myObject = lightTester(5)
-        myObject.apply(['switch', '296', '687', '906', '775'])
-        self.assertEqual(myObject.count(), -4, 'Failed: Count incorrect')
-
-
+    def test_count_functions(self):
+        test4 = lightTester(2)
+        test4.apply(['turn on', 0, 0, 1, 1])
+        self.assertTrue(test4.count() == 4)
+        
+        test5 = lightTester(3)
+        test5.apply(['switch', 0, 0, 2, 2])
+        self.assertTrue(test5.count() == 9)
+        
+        test6 = lightTester(5)
+        test6.apply(['turn off', 0, 0, 4, 4])
+        self.assertTrue(test6.count() == 0)
+        
+if __name__== '__main__':
+    unittest.main()
